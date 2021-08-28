a662ded7d84cd946bdfc7e5b0532b4555bb4aaf5
unknown <42Shermann>
2021-08-28 14:33:56 +0700

    Update Backend

--- backend/database.js
+++
@@ -1,7 +0,0 @@
-// Replace the following with values for your environment.
-const username = encodeURIComponent("dbSafty");
-const password = encodeURIComponent("atZf9dgE6aBX3uxu");
-const clusterUrl = "cluster0.t42yh.mongodb.net";
-const authMechanism = "DEFAULT";
-
-export default { username, password, clusterUrl, authMechanism };
